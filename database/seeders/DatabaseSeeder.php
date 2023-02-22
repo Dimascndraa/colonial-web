3a400d6a34460b3c40bf4fd1e055666de97f2c10
Dimascndraa <dimascndraa18@gmail.com>
2023-02-22 09:57:54 +0700

    Halaman Frontend dan Backend

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
             "logo_secondary" => "logo/rn2aR5FhgJK5KAY5hZBtTtJsJpPv403EvzjTIHHg.png",
             "icon" => "logo/icon/tMv14ArYeXzmTMueRUBkj2WobdlQ75DErCLcU1WS.png",
             "address" => "Jl. Brawijaya Desa No.01, RT.04/RW.08, Kadipaten, Kec. Kadipaten, Kabupaten Majalengka, Jawa Barat 45452",
-            "maps" => "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.0508156150363!2d108.16738675117112!3d-6.76365886799851!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f29ad694bb385%3A0xa5a96d4ab293e0a8!2sRedDoorz%20Syariah%20%40%20Kolonial%20Guest%20House!5e0!3m2!1sid!2sid!4v1676963649817!5m2!1sid!2sid",
+            "google_maps" => "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.0508156150363!2d108.16738675117112!3d-6.76365886799851!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f29ad694bb385%3A0xa5a96d4ab293e0a8!2sRedDoorz%20Syariah%20%40%20Kolonial%20Guest%20House!5e0!3m2!1sid!2sid!4v1676963649817!5m2!1sid!2sid",
             "telp" => "123",
         ]);
 
